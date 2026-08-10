3b97de16e8ad06fb703229990c2e0947a60608b0
tomitaro25 <132456822+tomitaro25@users.noreply.github.com>
2026-08-10 23:59:32 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
-const CACHE_NAME = 'boite-de-fiches-v12';
+const CACHE_NAME = 'boite-de-fiches-v13';
 const APP_SHELL = [
   './index.html',
   './manifest.json',
   './icon-192.png',
   './icon-512.png',
+  './icon-192-maskable.png',
+  './icon-512-maskable.png',
   './vocab-data.js'
 ];
 
